5806a3920a5522eb61f9725e18a4050394752974
JooHyun Kim <joohyun365@naver.com>
2026-08-27 03:47:25 +0900

    refactor: CommentController form to api

--- src/main/java/com/myDiary/demo/controller/CommentController.java
+++ src/main/java/com/myDiary/demo/controller/CommentController.java
@@ -1,41 +1,40 @@
 package com.myDiary.demo.controller;
 
 import com.myDiary.demo.dto.CommentRequestDto;
+import com.myDiary.demo.dto.CommentResponseDto;
 import com.myDiary.demo.service.CommentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
-@RequestMapping("/diaries/{diaryId}/comments")
+@RequestMapping("/api/diaries/{diaryId}/comments")
 public class CommentController {
     private final CommentService commentService;
 
     @PostMapping
-    public String addComment(@PathVariable Long diaryId, @Valid @ModelAttribute CommentRequestDto commentRequestDto, BindingResult bindingResult, @AuthenticationPrincipal UserDetails userDetails){
-        if (bindingResult.hasErrors()) {
-            return "redirect:/diaries/" + diaryId + "?error=empty_comment";
-        }
-        commentService.joinComment(commentRequestDto, diaryId, userDetails.getUsername());
-        return "redirect:/diaries/" + diaryId;
+    public ResponseEntity<CommentResponseDto> addComment(@PathVariable Long diaryId, @Valid @RequestBody CommentRequestDto commentRequestDto, @AuthenticationPrincipal UserDetails userDetails){
+
+        CommentResponseDto commentResponseDto = commentService.joinComment(commentRequestDto, diaryId, userDetails.getUsername());
+        return ResponseEntity.status(HttpStatus.CREATED).body(commentResponseDto);
     }
     @PutMapping("/{commentId}")
-    public String updateComment(@PathVariable Long diaryId, @PathVariable Long commentId, @Valid @ModelAttribute CommentRequestDto commentRequestDto, BindingResult bindingResult, @AuthenticationPrincipal UserDetails userDetails){
-        if (bindingResult.hasErrors()) {
-            return "redirect:/diaries/"+diaryId+"?error=empty_comment";
-        }
-        commentService.updateCommentById(commentId, commentRequestDto, userDetails.getUsername());
-        return "redirect:/diaries/" + diaryId;
+    public ResponseEntity<CommentResponseDto> updateComment(@PathVariable Long diaryId, @PathVariable Long commentId, @Valid @RequestBody CommentRequestDto commentRequestDto, @AuthenticationPrincipal UserDetails userDetails){
+
+        CommentResponseDto commentResponseDto = commentService.updateCommentById(commentId, commentRequestDto, userDetails.getUsername());
+        return ResponseEntity.status(HttpStatus.OK).body(commentResponseDto);
     }
 
     @DeleteMapping("/{commentId}")
-    public String deleteComment(@PathVariable Long diaryId, @PathVariable Long commentId, @AuthenticationPrincipal UserDetails userDetails){
+    public ResponseEntity<Void> deleteComment(@PathVariable Long diaryId, @PathVariable Long commentId, @AuthenticationPrincipal UserDetails userDetails){
         commentService.deleteComment(commentId, userDetails.getUsername());
-        return "redirect:/diaries/" + diaryId;
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
