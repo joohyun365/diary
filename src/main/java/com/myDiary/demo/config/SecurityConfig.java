6eb588b84a99abb1752c7b7210ea99adc8410b2a
JooHyun Kim <joohyun365@naver.com>
2026-09-03 01:31:09 +0900

    refactor: del useless comments

--- src/main/java/com/myDiary/demo/config/SecurityConfig.java
+++ src/main/java/com/myDiary/demo/config/SecurityConfig.java
@@ -33,16 +33,9 @@ public class SecurityConfig {
 
                 // URL별 접근 권한 설정 (인가)
                 .authorizeHttpRequests(auth -> auth
-                        // css, js 같은 정적 파일과 목록 조회, 로그인, 회원가입은 아무나 접근 가능
-                        // 메서드 단위로 허용
-                        /*
-                        .requestMatchers(HttpMethod.POST, "/api/members").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/diaries","/api/diaries/**").permitAll()
-                        .requestMatchers("/", "/login", "/css/**", "/error").permitAll()
-                        */
                         .requestMatchers(HttpMethod.GET, "/api/diaries","/api/diaries/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/members").permitAll()
-                        .requestMatchers("/api/diaries/**", "/api/login","/login").permitAll()
+                        .requestMatchers("/api/login","/login").permitAll()
                         // 그 외의 주소(글 작성, 수정, 삭제)는 무조건 로그인(인증)을 해야만 접근 가능
                         .anyRequest().authenticated()
                 )
